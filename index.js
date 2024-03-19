dc23934dee9ea6d98748f5b19895569dec85578a
Lily Yu <py561@nyu.edu>
2024-03-19 19:17:35 -0400

    created new scene with water, skybox, island

---
+++ index.js
@@ -0,0 +1,133 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
+
+import { Water } from "three/addons/objects/Water.js";
+import { Sky } from "three/addons/objects/Sky.js";
+
+let scene, camera, renderer;
+
+let controls, water, sun;
+
+function init() {
+  scene = new THREE.Scene();
+
+  let aspect = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+  camera.position.z = 5; // place the camera in space
+  camera.position.y = 1;
+  camera.lookAt(0, 0.7, 0);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // water
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "textures/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+
+  water.rotation.x = -Math.PI / 2;
+  scene.add(water);
+
+  // sun
+  sun = new THREE.Vector3();
+
+  // Skybox
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms["turbidity"].value = 10;
+  skyUniforms["rayleigh"].value = 2;
+  skyUniforms["mieCoefficient"].value = 0.005;
+  skyUniforms["mieDirectionalG"].value = 0.8;
+
+  const parameters = {
+    elevation: 5,
+    azimuth: 0,
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  const sceneEnv = new THREE.Scene();
+
+  let renderTarget;
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize();
+
+    if (renderTarget !== undefined) renderTarget.dispose();
+
+    sceneEnv.add(sky);
+    renderTarget = pmremGenerator.fromScene(sceneEnv);
+    scene.add(sky);
+
+    scene.environment = renderTarget.texture;
+  }
+
+  updateSun();
+
+  // add some lights so we can see our model
+  scene.add(new THREE.AmbientLight(0xffffff, 1));
+  scene.add(new THREE.DirectionalLight(0xffffff, 5));
+
+  window.addEventListener("resize", onWindowResize);
+
+  loadModel();
+
+  loop();
+}
+
+function loadModel() {
+  // first create a loader
+  let loader = new FBXLoader();
+
+  // then load the file and add it to your scene
+  loader.load("./island.fbx", function (object) {
+    object.scale.multiplyScalar(0.12);
+    object.position.set(0, -0.05, 0);
+    scene.add(object);
+  });
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function loop() {
+  window.requestAnimationFrame(loop); // pass the name of your loop function into this function
+
+  controls.update();
+
+  water.material.uniforms["time"].value += 0.1 / 60.0;
+
+  renderer.render(scene, camera);
+}
+
+init();
